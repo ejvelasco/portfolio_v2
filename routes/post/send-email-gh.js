18a710e6c99e67c46e350e3e7d0c6d72409b293a
velascoDev <dracontium2@gmail.com>
2017-06-25 13:25:31 -0500

    Added basic routes

---
+++ routes/post/send-email-gh.js
@@ -0,0 +1,41 @@
+module.exports = (app, nodemailer) => {
+	app.post("/send-email", (req, res) => {
+	    const details = req.body;
+	    const re = {
+	    	name: /^[a-zA-Z ]+$/,
+	    	email: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+	    	msg: /\S+/
+	    };
+	    let errors = false;
+	    for (const detail in details) {
+	    	if (!re[detail].test(details[detail])) {
+	    		res.json({error: detail});
+				errors = true;
+				break;
+	    	}
+	    }
+	    if (!errors) {
+		    const transporter = nodemailer.createTransport({
+		        service: "YOUR_EMAIL_SERVICE",
+		        auth: {
+		            user: "YOUR_EMAIL",
+		            pass: "YOUR_PASSWORD"
+		        }
+		    });
+		    transporter.sendMail({
+		        to: "DESTINATION_EMAIL",
+		        subject: "PORTFOLIO CONTACT",
+		        text: 
+		        	`Message: ${details.msg}
+		        	Name: ${details.name}
+		        	Email: ${details.email}`
+		    }, err => {
+		    	if (err && errors == false) {
+		    		res.json({error: "service"});
+		    	} else {
+		    		res.json({error: false});
+		    	}
+		    });
+		}
+  });
+};
